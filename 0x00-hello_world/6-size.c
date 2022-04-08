8b59f536e493622e2f4c9192024cee0e76813989
Adane <t.adane0055@gmail.com>
2022-04-08 07:46:28 -0700

    T6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -14,4 +13,3 @@ printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
-
