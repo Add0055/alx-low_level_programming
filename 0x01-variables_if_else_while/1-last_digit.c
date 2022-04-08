62c8ae6b5bffeb28c59462f581f349cd87a10135
Adane <t.adane0055@gmail.com>
2022-04-08 12:51:50 -0700

    t1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,7 @@
-#include <stdio.h>                                                                                                                      
-#include <stdlib.h>                                                                                                                     
-#include <time.h>                                                                                                                       
-
-/**                                                                                                                                     
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
  * main - entry point
  *
  * Return: Always 0 (success)
