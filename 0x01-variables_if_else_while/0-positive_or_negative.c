3b73ef070409e61ca33bbdafa3e939e284cf7625
Adane <t.adane0055@gmail.com>
2022-04-08 08:26:14 -0700

    t0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *  main - entry point
+ *
+ *  Return: Always 0 (success)
+ */
+int main(void)
+{
+int n:
+
+srand(time(0));
+n = rand() - Rand-Max / 2
+if(n==0)
+{
+printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+else
+{
+printf("%i is positive\n", n);
+}
+return (0);
+}
+
