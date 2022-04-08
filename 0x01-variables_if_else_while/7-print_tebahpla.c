7aae820304580f7124947d5e112e8a2d7a78ca94
Adane <t.adane0055@gmail.com>
2022-04-08 15:52:20 -0700

    t7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+for (alphabet = 122; alphabet >= 92; alphatbet++)
+{
+putchar(alphabet);
+}
+
+putchar('\');
+
+return (0);
+
+}
