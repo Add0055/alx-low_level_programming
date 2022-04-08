d92b77ff48a8cb177d643ac92a513080826ad31b
Adane <t.adane0055@gmail.com>
2022-04-08 13:57:42 -0700

    t2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
- * main - entry point 
+ * main - entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
-
 {
 char alphabet;
 
@@ -19,5 +16,4 @@ putchar(alphabet);
 putchar('\n');
 
 return (0);
-
 }
