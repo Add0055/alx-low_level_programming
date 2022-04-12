bc4916c15e3ec7ed441e0ed2c2be6e345645af5d
Adane <t.adane0055@gmail.com>
2022-04-12 13:40:11 -0700

    t1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - causes infinite loop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+
+i=0;
+/*
+ * while (i < 10)
+ * {
+ * infinite loop - No increase of variable
+ * putchar(i);
+ }
+ */
+printf("infinite loop avoided \\0/n\n");
+return (0);
+}
