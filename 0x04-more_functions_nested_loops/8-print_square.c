892172b0f6200eb784a35738da8035df0e001b61
Adane <t.adane0055@gmail.com>
2022-04-17 13:52:29 -0700

    t8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - prints hashtag to make a square
+ * @size: limit
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+int i, j;
+
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+putchar('#');
+}
+putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
