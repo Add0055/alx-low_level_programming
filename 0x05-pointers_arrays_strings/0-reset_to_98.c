07ed2b824a11eb5177ed9244cd394ee171752532
Adane <t.adane0055@gmail.com>
2022-04-18 05:06:50 -0700

    t0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - update integer value to 98
+ *@n: a pointer to an int that will be updated
+ *Return: void that means is correct
+ */
+
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
