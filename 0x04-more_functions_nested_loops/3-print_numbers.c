b8508de0fa232468b5b511b118836ad0c451a427
Adane <91440355+Add0055@users.noreply.github.com>
2022-04-19 03:48:18 -0700

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,17 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: Always 0 (success)
  */
 
 void print_numbers(void)
 {
-	int x;
+	int i;
 
-	for (x = 48; x < 58; x++)
-	{
-		_putchar(x);
-	}
-	_putchar(10);
+	for (i = 0; i < 10; i++)
+	
+		_putchar(i + '0');
+	
+	_putchar('\n');
 }
