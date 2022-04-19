85927c400c188f7e2c44459be0cc4d9841eff941
Adane <91440355+Add0055@users.noreply.github.com>
2022-04-19 03:50:12 -0700

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9
+ * print_numbers - prints the number zero to nine
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
 	int i;
+	
+	i = 0;
 
 	for (i = 0; i < 10; i++)
 	
