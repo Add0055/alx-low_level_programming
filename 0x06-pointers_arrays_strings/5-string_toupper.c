8f4bf27663fe812e5a3a00fd5a4642baf544cf48
Adane <91440355+Add0055@users.noreply.github.com>
2022-04-22 14:20:57 -0700

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,24 +1,17 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- *string_toupper - changes all lowercase letters of a string to uppercase.
- *@s: string to modify.
- *
- *Return: s modified.
- */
+ * *string_toupper - capitalize a string
+ * @str: pointer
+ * Return: capitalzied string
+*/
 
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] > 96 && s[i] < 123)
-		{
-			s[i] -= 32;
-		}
-		i++;
-	}
-
-	return (s);
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+	if (str[i] <= 'z' && str[i] >= 'a')
+		str[i] -= 32;
+}
+return (str);
 }
